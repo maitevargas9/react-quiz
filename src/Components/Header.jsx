078a003aba4367cab3ab432c2660d5822a12542a
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-02-25 15:45:04 +0100

    Add images and header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,9 @@
+import QuizLogo from "../assets/quizlogo.png";
+
 export default function Header() {
   return (
     <header>
+      <img src={QuizLogo} alt="Quiz Logo" />
       <h1>React Quiz</h1>
     </header>
   );
---
+++ src/Data/questions.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: "Q1",
+    question: "Welche ist die Hauptstadt von Spanien?",
+    answers: ["Madrid", "Bilbao", "Barcelona", "Santander"]
+  },
+  {
+    id: "Q2",
+    question: "Welche ist die Hauptstadt von Italien?",
+    answers: ["Rom", "Mailand", "Florenz", "Arezzo"]
+  },
+  {
+    id: "Q3",
+    question: "Welche ist die Hauptstadt von Griechenland?",
+    answers: ["Athen", "Olympia", "Monemvasia", "Kifisia"]
+  },
+  {
+    id: "Q4",
+    question: "Welche ist die Hauptstadt von Gross Britanien?",
+    answers: ["London", "Canterbury", "Oxford", "Glasgow"]
+  },
+  {
+    id: "Q5",
+    question: "Welche ist die Hauptstadt von Iceland?",
+    answers: ["Reykjavik", "Kópavogur", "Hafnarfjörður", "Keflavík"]
+  },
+  {
+    id: "Q6",
+    question: "Welche ist die Hauptstadt von Schweden?",
+    answers: ["Stockholm", "Göteborg", "Malmö", "Uppsala"]
+  },
+  {
+    id: "Q7",
+    question: "Welche ist die Hauptstadt von Deutschland?",
+    answers: ["Berlin", "Bonn", "Dusseldorf", "Munchen"]
+  }
+];
